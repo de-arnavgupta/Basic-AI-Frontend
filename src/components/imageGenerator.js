fa0d033868225a22e7b636d8ccb97ed492468fa8
noir <noir@odin.com>
2025-02-12 21:16:18 +0530

    added recipe generator

--- src/components/imageGenerator.js
+++ src/components/imageGenerator.js
@@ -5,6 +5,7 @@ function ImageGenerator() {
         <div>
             <h1>Image Generator</h1>
             <p>Generate images based on your preferences.</p>
+            <p><b><i>This feature is not supported as of now as Gemini doesn't provide support for image generation yet.</i></b></p>
         </div>
     );
 }
--- src/components/recipeGenerator.js
+++ src/components/recipeGenerator.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function RecipeGenerator() {
+
+    const [ingredients, setIngredients] = useState('');
+    const [cuisine, setCuisine] = useState('any');
+    const [dietaryRestrictions, setDietaryRestrictions] = useState('none');
+    const [recipe, setRecipe] = useState('');
+    const generateRecipe = async () => {
+        try{
+            const response = await fetch(`http://localhost:8080/createRecipe?ingredients=${ingredients}&cuisine=${cuisine}&diet=${dietaryRestrictions}`);
+            const data = await response.text();
+            setRecipe(data);
+            console.log(data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+
     return (
         <div>
             <h1>Recipe Generator</h1>
             <p>Generate recipes based on your preferences.</p>
+            <input type="text"
+                value = {ingredients}
+                onChange={(e) => setIngredients(e.target.value)}
+                placeholder={"Enter ingredients (comma separated)"}
+            />
+            <input type="text"
+                value={cuisine}
+                onChange={(e) => setCuisine(e.target.value)}
+                placeholder={"Enter cuisine"}
+            />
+            <input type="text"
+                value={dietaryRestrictions}
+                onChange={(e) => setDietaryRestrictions(e.target.value)}
+                placeholder={"Enter dietary restrictions"}
+            />
+            <button onClick={generateRecipe}> Generate Recipe</button>
+            <div className="output">
+                <pre className="recipe-text">{recipe}</pre>
+            </div>
         </div>
     );
 }
